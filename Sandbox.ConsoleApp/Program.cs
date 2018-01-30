ec70b2c2ed058ee3ddad19b20fba9c06d5d46df4
Grzegorz Krawczyszyn <gkrawczyszyn@gmail.com>
2018-01-30 07:44:04 +0100

    Initialize Serilog console logging

--- Sandbox.ConsoleApp/Program.cs
+++ Sandbox.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Serilog;
+using System;
 using Sandbox.BusinessLogic;
 
 namespace Sandbox.ConsoleApp
@@ -7,12 +8,26 @@ namespace Sandbox.ConsoleApp
     {
         static void Main(string[] args)
         {
+            InitializeLogger();
+            Log.Information("Logger initialized");
+
             Console.WriteLine("What's your name?");
 
             var name = Console.ReadLine();
             var response = HelloWorld.Welcome(name);
 
             Console.WriteLine(response);
+
+            Log.Information("App finished");
+        }
+
+        private static void InitializeLogger()
+        {
+            Serilog.ILogger logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+            
+            Log.Logger = logger;
         }
     }
 }
